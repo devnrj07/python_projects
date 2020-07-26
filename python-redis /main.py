f05094f62ca7660e48f91c5cf0a08910722542e3
devnrj07 <niraj.mukta123@gmail.com>
2020-07-26 19:49:06 +0530

    pyhats.com initial commit

--- python-redis /main.py
+++ python-redis /main.py
@@ -1,4 +1,39 @@
-import redis 
-r = redis.Redis()
-r.mset({"Master":"Slave", "Key":"Value"})
-r.get("Key")
+import redis
+import logging
+
+r = redis.Redis('localhost',6379)
+
+
+logging.basicConfig()
+
+class OutOfStockError(Exception):
+    """Raised when PyHats.com is all out of today's hottest hat"""
+
+def buyitem(r: redis.Redis, itemid: int) -> None:
+    with r.pipeline() as pipe:
+        error_count = 0
+        while True:
+            try:
+                #Get available inventory, watching for changes
+                # related to this itemid before the transaction
+                pipe.watch(itemid)
+                nleft: bytes = r.hget(itemid, "quantity")
+                if nleft > b"0":
+                    pipe.multi()
+                    pipe.hincrby(itemid, -1)
+                    pipe.hincrby(itemid, 1)
+                    pipe.execute()
+                    break
+                else:
+                    #stop watching the itemid and raise to break out
+                    pipe.unwatch()
+                    raise OutOfStockError(
+                            f"Sorry, {itemid} is out of stock!")
+            except redis.WatchError:
+                #Log total num. of errors by this user to buy this item,
+                # then try the same process again of WATCH/HGET/MULTI/EXEC
+                error_count += 1
+                logging.warning(
+                        "WatchError #%d: %s; retrying",
+                        error_count,itemid)
+                return None
